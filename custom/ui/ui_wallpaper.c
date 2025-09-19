d9f9b37a5c2874c83894f1195faa7e94e8434c8b
Sameer .I. Siddiqui <admin@babahumor.com>
2025-09-19 16:41:41 +0530

    feat(ui): render wallpaper with launcher asset

--- custom/ui/ui_wallpaper.c
+++ custom/ui/ui_wallpaper.c
@@ -5,20 +5,25 @@
  */
 #include "ui_wallpaper.h"
 
-ui_wallpaper_t *ui_wallpaper_attach(lv_obj_t *parent)
+#include "app/assets/assets.h"
+
+ui_wallpaper_t* ui_wallpaper_attach(lv_obj_t* parent)
 {
-    if (parent == NULL) {
+    if (parent == NULL)
+    {
         return NULL;
     }
 
-    ui_wallpaper_t *wallpaper = (ui_wallpaper_t *)lv_malloc(sizeof(ui_wallpaper_t));
-    if (wallpaper == NULL) {
+    ui_wallpaper_t* wallpaper = (ui_wallpaper_t*)lv_malloc(sizeof(ui_wallpaper_t));
+    if (wallpaper == NULL)
+    {
         return NULL;
     }
     lv_memset(wallpaper, 0, sizeof(ui_wallpaper_t));
 
     wallpaper->layer = lv_obj_create(parent);
-    if (wallpaper->layer == NULL) {
+    if (wallpaper->layer == NULL)
+    {
         lv_free(wallpaper);
         return NULL;
     }
@@ -27,22 +32,41 @@ ui_wallpaper_t *ui_wallpaper_attach(lv_obj_t *parent)
     lv_obj_set_size(wallpaper->layer, LV_PCT(100), LV_PCT(100));
     lv_obj_add_flag(wallpaper->layer, LV_OBJ_FLAG_IGNORE_LAYOUT);
     lv_obj_move_background(wallpaper->layer);
-    lv_obj_set_style_bg_opa(wallpaper->layer, LV_OPA_COVER, LV_PART_MAIN);
-    lv_obj_set_style_bg_color(wallpaper->layer, lv_color_hex(0xfcff9e), LV_PART_MAIN);
-    lv_obj_set_style_bg_grad_color(wallpaper->layer, lv_color_hex(0xc67700), LV_PART_MAIN);
-    lv_obj_set_style_bg_grad_dir(wallpaper->layer, LV_GRAD_DIR_HOR, LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(wallpaper->layer, LV_OPA_TRANSP, LV_PART_MAIN);
     lv_obj_set_style_border_width(wallpaper->layer, 0, LV_PART_MAIN);
+
+    wallpaper->image = lv_image_create(wallpaper->layer);
+    if (wallpaper->image == NULL)
+    {
+        lv_obj_del(wallpaper->layer);
+        lv_free(wallpaper);
+        return NULL;
+    }
+
+    lv_obj_remove_style_all(wallpaper->image);
+    lv_image_set_src(wallpaper->image, &launcher_bg);
+    lv_obj_set_align(wallpaper->image, LV_ALIGN_CENTER);
+    lv_obj_add_flag(wallpaper->image, LV_OBJ_FLAG_IGNORE_LAYOUT);
+    lv_obj_move_background(wallpaper->image);
 
     return wallpaper;
 }
 
-void ui_wallpaper_detach(ui_wallpaper_t *wallpaper)
+void ui_wallpaper_detach(ui_wallpaper_t* wallpaper)
 {
-    if (wallpaper == NULL) {
+    if (wallpaper == NULL)
+    {
         return;
     }
 
-    if (wallpaper->layer != NULL) {
+    if (wallpaper->image != NULL)
+    {
+        lv_obj_del(wallpaper->image);
+        wallpaper->image = NULL;
+    }
+
+    if (wallpaper->layer != NULL)
+    {
         lv_obj_del(wallpaper->layer);
         wallpaper->layer = NULL;
     }
--- custom/ui/ui_wallpaper.h
+++ custom/ui/ui_wallpaper.h
@@ -6,29 +6,32 @@
 #pragma once
 
 #ifdef __has_include
-#if __has_include("lvgl.h")
-#ifndef LV_LVGL_H_INCLUDE_SIMPLE
-#define LV_LVGL_H_INCLUDE_SIMPLE
-#endif
-#endif
+#    if __has_include("lvgl.h")
+#        ifndef LV_LVGL_H_INCLUDE_SIMPLE
+#            define LV_LVGL_H_INCLUDE_SIMPLE
+#        endif
+#    endif
 #endif
 
 #if defined(LV_LVGL_H_INCLUDE_SIMPLE)
-#include "lvgl.h"
+#    include "lvgl.h"
 #else
-#include "lvgl/lvgl.h"
+#    include "lvgl/lvgl.h"
 #endif
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-typedef struct ui_wallpaper_t {
-    lv_obj_t *layer;
-} ui_wallpaper_t;
+    typedef struct ui_wallpaper_t
+    {
+        lv_obj_t* layer;
+        lv_obj_t* image;
+    } ui_wallpaper_t;
 
-ui_wallpaper_t *ui_wallpaper_attach(lv_obj_t *parent);
-void ui_wallpaper_detach(ui_wallpaper_t *wallpaper);
+    ui_wallpaper_t* ui_wallpaper_attach(lv_obj_t* parent);
+    void            ui_wallpaper_detach(ui_wallpaper_t* wallpaper);
 
 #ifdef __cplusplus
 }
